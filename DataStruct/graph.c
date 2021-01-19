25da996c455b68c41c3a651d295caf035e2e04b5
matheus1714 <matheussilvamartins1714@gmail.com>
2021-01-19 19:52:26 -0300

    implemented basic graph operations

---
+++ DataStruct/graph.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef int TIPOPESO;
+
+typedef struct adjacencia{
+    int vertice;
+    TIPOPESO peso;
+    struct adjacencia *prox;
+}ADJACENCIA;
+
+typedef struct vertice{
+    ADJACENCIA *cab;
+}VERTICE;
+
+typedef struct grafo{
+    int vertices;
+    int arestas;
+    VERTICE *adj;
+}GRAFO;
+
+GRAFO *criarGrafo(int v){
+    GRAFO *g = (GRAFO*)malloc(sizeof(GRAFO));
+    g->vertices = v;
+    g->arestas = 0;
+    g->adj = (VERTICE* )malloc(v*sizeof(VERTICE));
+    int i;
+    for(i = 0; i < v; i++) g->adj[i].cab = NULL;
+    return g;
+}
+
+ADJACENCIA *criaAdj(int v, int peso){
+    ADJACENCIA *temp = (ADJACENCIA *)malloc(sizeof(ADJACENCIA));
+    temp->vertice = v;
+    temp->peso = peso;
+    temp->prox = NULL;
+    return temp;
+}
+
+bool criaAresta(GRAFO *gr, int vi, int vf, TIPOPESO p){
+    if(!gr) return false;
+    if((vf<0) || (vf >= gr->vertices)) return false;
+    if((vi<0) || (vi >= gr->vertices)) return false;
+    ADJACENCIA *novo = criaAdj(vf, p);
+    novo->prox = gr->adj[vi].cab;
+    gr->adj[vi].cab = novo;
+    gr->arestas++;
+    return true;
+}
+
+void imprime(GRAFO *gr){
+    printf("Vertices %d. Arestas: %d. \n", gr->vertices, gr->arestas);
+    int i;
+    for(i=0; i<gr->vertices; i++){
+        printf("v%d: ", i);
+        ADJACENCIA *ad = gr->adj[i].cab;
+        while(ad){
+            printf("v%d(%d) ", ad->vertice, ad->peso);
+            ad = ad->prox;
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    GRAFO *gr = criarGrafo(5);
+    criaAresta(gr, 0, 1, 2);
+    criaAresta(gr, 1, 2, 4);
+    criaAresta(gr, 2, 0, 12);
+    criaAresta(gr, 2, 4, 40);
+    criaAresta(gr, 3, 1, 3);
+    criaAresta(gr, 4, 3, 8);
+
+    imprime(gr);
+    return 0;
+}
